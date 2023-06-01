26584bd748acc816fbeb4543f639c985ece9988f
Administrator <Administrator@DESKTOP-2VLVNHN>
2023-06-01 11:06:11 +0900

    여준호님 파일 풀받기 위한 커밋

--- GgiriProject/src/main/java/com/ggiri/root/chat/server/ChatServer.java
+++ GgiriProject/src/main/java/com/ggiri/root/chat/server/ChatServer.java
@@ -4,10 +4,13 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
+
 
 @ServerEndpoint("/chatserver")
 public class ChatServer {
@@ -24,7 +27,58 @@ public class ChatServer {
 	}
 	
 	@OnMessage
-	public void handleMessage(String msg, Session session) {
+	public void handleMessage(String message, Session session) {
+		
+		int index = message.indexOf("#", 2);
+		String no = message.substring(0, 1);
+		String user = message.substring(2, index);
+		String txt = message.substring(index + 1);
+		
+		if(no.equals("1")) {
+			for(Session s : list) {
+				if(s != session) {
+					try {
+						s.getBasicRemote().sendText("1#" + user + "#");
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		} else if(no.equals("2")) {
+			
+			for(Session s : list) {
+				if(s != session) {
+					try {
+						s.getBasicRemote().sendText("2#" + user + " : " + txt);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}
+			
+		} else if(no.equals("3")) {
+			
+			for(Session s : list) {
+				if(s != session) {
+					try {
+						s.getBasicRemote().sendText("3#" + user + "#");
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}
+			list.remove(session);
+		}
+		
+	}
+	
+	@OnClose
+	public void handleClose() {
+		
+	}
+	
+	@OnError
+	public void handleError(Throwable t) {
 		
 	}
 }
