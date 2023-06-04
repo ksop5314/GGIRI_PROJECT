0cc263e5331443aa97597347fa17bc2cbf690536
ksop6 <ksop6@doono>
2023-06-04 17:53:19 +0900

    카카오 로그인 기능 완료

--- GgiriProject/src/main/java/com/ggiri/root/kakao/service/KakaoService.java
+++ GgiriProject/src/main/java/com/ggiri/root/kakao/service/KakaoService.java
@@ -2,10 +2,14 @@ package com.ggiri.root.kakao.service;
 
 import java.util.Map;
 
+import com.ggiri.root.kakao.vo.SessionConfigVO;
+
 public interface KakaoService {
 	
 	public String getReturnAccessToken(String code);
 	public Map<String, Object> getUserInfo(String access_token);
 	public void getLogout(String access_token);
+	public int findKakao(SessionConfigVO vo);
+	public void kakaoinsert(SessionConfigVO vo);
 	
 }
